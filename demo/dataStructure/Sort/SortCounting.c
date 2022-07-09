d80aaf3d8762ee5970071f9d8fca1362dc6c34e9
zdj03 <15007102372@163.com>
2022-07-09 08:27:26 +0800

    分治算法求无序数组中无序对

---
+++ demo/dataStructure/Sort/SortCounting.c
@@ -0,0 +1,8 @@
+//
+//  SortCounting.c
+//  Sort
+//
+//  Created by 周登杰 on 2022/7/9.
+//
+
+#include "SortCounting.h"
---
+++ demo/dataStructure/Sort/SortCounting.h
@@ -0,0 +1,13 @@
+//
+//  SortCounting.h
+//  Sort
+//
+//  Created by 周登杰 on 2022/7/9.
+//
+
+#ifndef SortCounting_h
+#define SortCounting_h
+
+#include <stdio.h>
+
+#endif /* SortCounting_h */
